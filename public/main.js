d21bdb3ad8626839697cc77c76d6cfcd1cb1d974
Jim Strother <jim.strother@gmail.com>
2016-05-20 22:20:24 -0700

    i think i have the nickname added right, but it's using jQuery, not Socket.io

--- public/main.js
+++ public/main.js
@@ -1,15 +1,33 @@
 $(document).ready(function() {
 	const socket = io();
-	const input = $('input');
+	const name = $('#name');
+	const input = $('#message');
 	const messages = $('#messages');
 
+	var user;
+
+	name.on('keydown', function(event) {
+		user = name.val();
+
+		if (event.keyCode != 13) {
+			return;
+		} else {
+			$('#nameDiv').hide();
+			$('#messageDiv').show();
+		}
+
+		user += ' says\: ';
+		console.log(user);
+		return user;
+	});
 	input.on('keydown', function(event) {
-		const message = input.val();
+		const text = input.val();
 
 		if (event.keyCode != 13) {
 			return;
 		}
 
+		message = user + text;
 		addMessge(message);
 		socket.emit('message', message);
 		input.val('');
@@ -18,6 +36,6 @@ $(document).ready(function() {
 	socket.on('message', addMessge);
 
 	function addMessge(message) {
-		messages.append('<div>' + message + '</div>');
+		messages.append('<div>' + message + '</div><br><br>');
 	};
 });
